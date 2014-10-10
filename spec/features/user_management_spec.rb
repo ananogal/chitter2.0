62dad6262a1dcc246d0d148be5968c5e3c79e143
ananogal <ana.nogal@gmail.com>
2014-10-10 15:44:39 +0100

    Move methods from user_management_spec into sessions.rb

--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+require_relative 'helpers/sessions'
 
 feature "User Signs in" do
 
@@ -35,23 +36,6 @@ feature "User Signs in" do
 		visit '/'
 		expect{sign_in("test@test.com", "")}.to change(User, :count).by(0)
 		expect(page).to have_content("The field name is mandatory")
-	end
-
-	def sign_in(email="test@test.com", name="Test", username="test_test", 
-					password = "test", password_confirmation="test")
-		visit '/'
-		click_link("Sign up")
-		fill_in :email,  :with => email
-		fill_in :name,  :with => name
-		fill_in :username,  :with => username
-		fill_in :password,  :with => password
-		fill_in :password_confirmation, :with => password_confirmation
-		click_button("Sign up")
-	end
-
-	def sign_out 
-		sign_in
-		click_button('Sign out')
 	end
 end
 
