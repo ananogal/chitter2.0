612b6f68fc2fdfb2c5301470170a9de332b81de9
ananogal <ana.nogal@gmail.com>
2014-10-10 15:26:58 +0100

    refactoring tests to pass last test

--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -3,30 +3,23 @@ require 'spec_helper'
 feature "User Signs in" do
 
 	scenario "As a user I want to sign up" do
-		visit '/'
 		sign_in
 		expect(page).to have_content('Welcome, Test')
 		expect(User.first.email).to eq("test@test.com")
 	end
 
 	scenario "As a user I must enter an email" do
-		visit '/'
 		expect{sign_in("")}.to change(User, :count).by(0)
 		expect(page).to have_content("The field email is mandatory")
 	end
 
 	scenario "As a user I want to logout" do
-		visit'/'
-		sign_in
-		click_button('Sign out')
-		save_and_open_page
+		sign_out
 		expect(page).to have_content("Good bye!")
 	end
 
 	scenario "As a user I must enter a unique email" do
-		visit '/'
-		sign_in
-		visit '/'
+		sign_out
 		expect{sign_in("test@test.com")}.to change(User, :count).by(0)
 		expect(page).to have_content("This email is already taken")
 	end
@@ -38,14 +31,14 @@ feature "User Signs in" do
 	end
 
 	scenario "As a user I must enter a unique username" do
-		visit '/'
-		expect{sign_in("")}.to change(User, :count).by(0)
+		sign_out
+		expect{sign_in("test2@test.com", "Test2", "test_test")}.to change(User, :count).by(0)
 		expect(page).to have_content("This username is already taken")
 	end
 
 	def sign_in(email="test@test.com", name="Test", username="test_test", 
 					password = "test", password_confirmation="test")
-
+		visit '/'
 		click_link("Sign up")
 		fill_in :email,  :with => email
 		fill_in :name,  :with => name
@@ -53,5 +46,10 @@ feature "User Signs in" do
 		fill_in :password,  :with => password
 		fill_in :password_confirmation, :with => password_confirmation
 		click_button("Sign up")
+	end
+
+	def sign_out 
+		sign_in
+		click_button('Sign out')
 	end
 end
