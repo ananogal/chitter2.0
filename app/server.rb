a384d35f1c451d0f9f4ac233355ed1fb58eb0bd9
ananogal <ana.nogal@gmail.com>
2014-10-10 13:13:20 +0100

    start test for users: added user model. added views for index and users

--- app/server.rb
+++ app/server.rb
@@ -1,6 +1,12 @@
 require 'sinatra'
 require 'data_mapper'
+require 'rack-flash'
 
-get '/' do
-	
-end
+require_relative "data_mapper_setup"
+
+require_relative "controllers/application"
+require_relative "controllers/users"
+
+use Rack::Flash, :sweep =>true
+enable :sessions
+set :partial_template_engine, :erb
---
+++ spec/features/user_management_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+feature "User Signs in" do
+
+	scenario "As a user I want to sign in" do
+		visit '/'
+		click_button("Sign in")
+		visit '/users/new'
+		fill_in :name,  :with => "name"
+		fill_in :username,  :with => "username"
+		fill_in :email,  :with => "email"
+		fill_in :password,  :with =>"password"
+		fill_in :password_confirmation, :with => "password"
+		click_button("Sign up")
+		expect(page).to have_content('Welcome, name')
+		expect(User.first.email).to eq("email")
+	end
+end
